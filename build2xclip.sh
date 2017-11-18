2ed4124240c1a1954442d69fbe315aefd06dee40
maartyl <kuronomaartyl@gmail.com>
2017-11-18 18:47:08 +0100

    update for .user.js extension

--- build2xclip.sh
+++ build2xclip.sh
@@ -1,2 +1,4 @@
-coffee -c scroll_drag.coffee && cat header.js scroll_drag.js | xcs
-xcg > scroll_drag.js
+coffee -c scroll_drag.coffee &&
+mv scroll_drag.js scroll_drag.user.js
+cat header.js scroll_drag.user.js | xcs &&
+xcg > scroll_drag.user.js
